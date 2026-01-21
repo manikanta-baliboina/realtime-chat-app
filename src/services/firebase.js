4fe57050d8a86d4ecb6b87c540fe30f94efdf6ef
manikanta-baliboina <manikanta1610598@gmail.com>
2026-01-21 12:34:57 +0530

    Fix Firebase env configuration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,22 +1,17 @@
-// src/services/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4_Bniq69J8yNCN1QT11LMX_uvTblFGsA",
-  authDomain: "realtime-chat-app-d4d6f.firebaseapp.com",
-  projectId: "realtime-chat-app-d4d6f",
-  storageBucket: "realtime-chat-app-d4d6f.appspot.com",
-  messagingSenderId: "619203074017",
-  appId: "1:619203074017:web:9cb4e8d14aae883050a4a0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// EXPORT THESE (VERY IMPORTANT)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
